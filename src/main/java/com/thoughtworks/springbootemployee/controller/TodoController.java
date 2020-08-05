7f39a2d4cd87580bd056df42e63aff758178cfc2
fox <1036674617@qq.com>
2020-08-05 22:24:42 +0800

    [York] feature: add controller mapping

--- src/main/java/com/thoughtworks/springbootemployee/controller/TodoController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/TodoController.java
@@ -1,12 +1,34 @@
 package com.thoughtworks.springbootemployee.controller;
 
 
+import com.thoughtworks.springbootemployee.model.Todo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
+
+    @GetMapping
+    public List<Todo> getTodoList() {
+        return null;
+    }
+
+    @PostMapping
+    public Todo addTodo() {
+        return null;
+    }
+
+    @DeleteMapping
+    public Todo deleteTodo() {
+        return null;
+    }
+
+    @PutMapping
+    public Todo changeStatus() {
+        return null;
+    }
 
 }
