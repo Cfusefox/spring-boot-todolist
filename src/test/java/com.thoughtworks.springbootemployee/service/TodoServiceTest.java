c36f1d535a104a0d6ad37c8bc654b4791ae83cf9
fox <1036674617@qq.com>
2020-08-05 22:21:43 +0800

    [York] feature: add should_return_todo_list_when_get_todo_list_given_null test case

--- src/test/java/com.thoughtworks.springbootemployee/service/TodoServiceTest.java
+++ src/test/java/com.thoughtworks.springbootemployee/service/TodoServiceTest.java
@@ -1,17 +1,33 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.model.Todo;
+import com.thoughtworks.springbootemployee.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class TodoServiceTest {
 
     @Test
-    void should__when__given() {
+    void should_return_todo_list_when_get_todo_list_given_null() {
         //given
-
+        List<Todo> todos = Arrays.asList(
+           new Todo(1, "test1", true),
+           new Todo(2, "test2", false)
+        );
+        TodoRepository mockTodoReposition = mock(TodoRepository.class);
+        TodoService todoService = new TodoService(mockTodoReposition);
+        given(mockTodoReposition.findAll()).willReturn(todos);
 
         //when
-
+        List<Todo> todoList = todoService.getTodoList();
 
         //then
+        assertEquals(todos, todoList);
     }
 }
