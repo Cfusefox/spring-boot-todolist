ef088c822f2246a1f0539c01c769be2eb94c04ce
fox <1036674617@qq.com>
2020-08-06 22:40:00 +0800

    [York] feature: add cross-domain config

---
+++ src/main/java/com/thoughtworks/springbootemployee/config/globalCrossConfig.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.springbootemployee.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
+
+@Configuration
+public class globalCrossConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
